4ce09a4d7febcb3e018c603131bb55410cd29ca4
Masha Kasatkina <mariia.kasatkina@dif.tech>
2024-11-22 16:51:11 +0400

    review fixes

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,21 +2,19 @@ package com.example.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton = findViewById<Button>(R.id.settings_back_button)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         val mainActivity = Intent(this, MainActivity::class.java)
 
-        backButton.setOnClickListener {
+        toolbar.setNavigationOnClickListener {
             startActivity(mainActivity)
         }
     }
