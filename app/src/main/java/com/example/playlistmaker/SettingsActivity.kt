9050eb9e6100e19a8ff11e2435e04f449ccf9d7e
Masha Kasatkina <mariia.kasatkina@dif.tech>
2024-12-03 21:21:34 +0400

    added intents

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,12 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textview.MaterialTextView
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -17,5 +20,42 @@ class SettingsActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             startActivity(mainActivity)
         }
+
+        val shareButton = findViewById<MaterialTextView>(R.id.setting_item_share)
+        val contactSupport = findViewById<MaterialTextView>(R.id.setting_item_contact_support)
+        val userAgreement = findViewById<MaterialTextView>(R.id.setting_item_user_agreement)
+
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "https://practicum.yandex.kz/learn/android-developer-plus/courses/"
+            )
+            startActivity(intent)
+        }
+
+        contactSupport.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(
+                Intent.EXTRA_EMAIL,
+                "patrick.delroy.yohoho@gmail.com"
+            )
+            intent.putExtra(
+                Intent.EXTRA_SUBJECT,
+                "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            )
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Спасибо разработчикам и разработчицам за крутое приложение!"
+            )
+            startActivity(intent)
+        }
+
+        userAgreement.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(intent)
+        }
+
     }
 }
