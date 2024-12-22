29bc9d23e0af9ba311445813097e5b1deb35dc57
Masha Kasatkina <mariia.kasatkina@dif.tech>
2024-12-22 17:16:09 +0400

    track adapter + track view holder + placeholder + night/light placeholder theme

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -5,13 +5,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SearchActivity : AppCompatActivity() {
 
@@ -21,11 +23,18 @@ class SearchActivity : AppCompatActivity() {
     private val toolbar by lazy { findViewById<Toolbar>(R.id.search_toolbar)}
 
     val clearButton: ImageView by lazy { findViewById(R.id.clearIcon)}
+    lateinit var adapter: TrackAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         val mainActivity = Intent(this, MainActivity::class.java)
+        val songListRecycler: RecyclerView = findViewById(R.id.song_list_recycler)
+        songListRecycler.layoutManager = LinearLayoutManager(this)
+
+
+        adapter = TrackAdapter(TRACKS)
+        songListRecycler.adapter = adapter
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -83,5 +92,37 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         const val SEARCH_TEXT = "SEARCH_TEXT"
         const val EMPTY_SEARCH_TEXT = ""
+        val TRACKS: ArrayList<Track> = arrayListOf(
+            Track(
+                "Smells Like Teen Spirit",
+                "Nirvana",
+                "5:01",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Billie Jean",
+                "Michael Jackson",
+                "4:35",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Stayin' Alive",
+                "Bee Gees",
+                "4:10",
+                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Whole Lotta Love",
+                "Led Zeppelin",
+                "5:33",
+                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Sweet Child O'Mine",
+                "Guns N' Roses",
+                "5:03",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+            )
+        )
     }
 }
