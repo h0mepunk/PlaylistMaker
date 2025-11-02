cb60cf3a2052d04cd9dc3aa6eb7447b880433d20
masha.kasatkina <mariia.kasatkina@dif.tech>
2025-11-02 15:46:02 +0400

    mvp refactoring almost done with tracksPresenter

--- app/src/main/java/com/example/playlistmaker/presentation/search/TracksSearchPresenter.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/search/TracksSearchPresenter.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.playlistmaker.Const
@@ -14,13 +13,11 @@ import com.example.playlistmaker.R
 import com.example.playlistmaker.domain.api.TracksHistoryInteractor
 import com.example.playlistmaker.domain.api.TracksInteractor
 import com.example.playlistmaker.domain.models.Track
-import com.example.playlistmaker.ui.track.TrackAdapter
 import com.example.playlistmaker.util.Creator
 
 class TracksSearchPresenter(
     val view: TracksSearchView,
     private val context: Context,
-    private var adapter: TrackAdapter
 ) {
     private var tracksInteractor: TracksInteractor = Creator.provideTracksInteractor(context)
     companion object {
@@ -37,17 +34,7 @@ class TracksSearchPresenter(
     var trackHistoryInteractor : TracksHistoryInteractor = Creator.provideTracksHistoryInteractor()
 
     fun onCreate(savedInstanceState: Bundle?) {
-       // adapter = TrackAdapter()
-
-        adapter.items = trackList
-
-//        trackListRecycler.layoutManager =
-//            LinearLayoutManager(
-//                activity,
-//                LinearLayoutManager.VERTICAL,
-//                false
-//            )
-//        trackListRecycler.adapter = adapter
+        view.updateTracksList(trackList)
 
         tracksInteractor = Creator.provideTracksInteractor(context)
         sharedPreferences = context.getSharedPreferences(
@@ -56,64 +43,7 @@ class TracksSearchPresenter(
         )
         trackHistoryInteractor = Creator.provideTracksHistoryInteractor()
         showHistory()
-//        placeholderMessage.visibility = View.GONE
-//        clearButton.isVisible = false
-//        val inputMethodManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-//
-//        refreshButton.setOnClickListener {
-//            progressBar.visibility = View.VISIBLE
-//            loadTracks(textDump.toString())
-//        }
-//        clearButton.setOnClickListener {
-//            inputEditText.setText(EMPTY_SEARCH_TEXT)
-//            showHistory()
-//            inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
-//            trackList.clear()
-//            adapter.items = trackList
-//            adapter.notifyDataSetChanged()
-//        }
-//        toolbar.setNavigationOnClickListener {
-//            activity.finish()
-//        }
-//        clearTrackHistoryButton.setOnClickListener {
-//            trackHistory.clear()
-//            trackHistoryInteractor.saveTracksHistory(trackHistory)
-//            hideHistory()
-//        }
-//
-//        inputEditText.setOnFocusChangeListener() { _, hasFocus -> }
-//        inputEditText.setOnEditorActionListener { _, actionId, _ ->
-//            if (actionId == EditorInfo.IME_ACTION_DONE) {
-//                if (inputEditText.text.isNotEmpty()) {
-//                    inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
-//                    textDump = inputEditText.text
-//                    loadTracks(textDump.toString())
-//                }
-//            }
-//            false
-//        }
-//        inputEditText.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                textDump = s
-//                searchDebounce(textDump.toString())
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {
-//                if (s.isNullOrEmpty()) {
-//                    Log.e("TracksSearchController", "all callbacks removed")
-//                    handler.removeCallbacksAndMessages(null)
-//                    showHistory()
-//                }
-//                if((inputEditText.hasFocus()) && s.isNullOrEmpty()) {
-//                    showHistory()
-//                }
-//            }
-//
-//        })
-//
+
         if (savedInstanceState != null) {
             lastSearchText = savedInstanceState.getCharSequence(
                 SEARCH_TEXT,
@@ -160,38 +90,33 @@ class TracksSearchPresenter(
             tracksInteractor.searchTracks(newSearchText, object: TracksInteractor.TracksConsumer {
                 override fun consume(foundTracks: List<Track>?, errorMessage: String?) {
                     Log.e("TracksSearchController", "foundTracks: $foundTracks")
-                    if (foundTracks != null) {
-                       //activity.runOnUiThread {
+                    handler.post {
+                        if (foundTracks != null) {
                             view.showProgressBar(false)
                             hideMessage()
                             trackList.clear()
                             trackList.addAll(foundTracks)
-                            adapter.items = trackList
-                            adapter.notifyDataSetChanged()
-                            view.showTracksList(true)//trackListRecycler.visibility = View.VISIBLE
+                            view.updateTracksList(trackList)
+                            view.showTracksList(true)
                             hideHistory()
-                     //   }
-                    }
-                    if (errorMessage != null) {
-                     //  context.runOnUiThread {
+                        }
+                        if (errorMessage != null) {
                             showMessage(
                                 text = R.string.network_error_text,
                                 additionalMessage = errorMessage,
                                 isButtonVisible = false,
                                 icon = R.drawable.internet_error
                             )
-                     //   }
-                    } else if (foundTracks == null) {
-                       // activity.runOnUiThread {
+                        } else if (foundTracks.isNullOrEmpty()) {
                             showMessage(
                                 text = R.string.empty_song_list_error_text,
                                 additionalMessage = "",
                                 isButtonVisible = false,
                                 icon = R.drawable.empty_results_error
                             )
-                     //   }
-                    } else {
-                        hideMessage()
+                        } else {
+                            hideMessage()
+                        }
                     }
                 }
                 // Add error response handling later
@@ -227,16 +152,18 @@ class TracksSearchPresenter(
         isButtonVisible: Boolean = true,
         icon: Int = R.drawable.internet_error
     ) {
-
         trackList.clear()
-        adapter.items = trackList
-        adapter.notifyDataSetChanged()
+        view.updateTracksList(trackList)
         hideHistory()
         if (text != null) {
+            Log.e("TracksSearchController", "showMessage: $text")
             view.changePlaceholderMessage(ContextCompat.getString(context, text))
             if (additionalMessage.isNotEmpty()) {
-                Toast.makeText(context, additionalMessage, Toast.LENGTH_LONG)
-                    .show()
+                Log.e("TracksSearchController", "showToast: $additionalMessage")
+                handler.post {
+                    Toast.makeText(context, additionalMessage, Toast.LENGTH_LONG)
+                        .show()
+                }
             }
 
             view.showPlaceholderMessage(true)
@@ -262,8 +189,7 @@ class TracksSearchPresenter(
         if (trackHistory.isNotEmpty()) {
             view.showHistory(true)
             hideMessage()
-            adapter.items = trackHistory
-            adapter.notifyDataSetChanged()
+            view.updateTracksList(trackHistory)
             view.showTracksList(true)
         } else {
             hideHistory()
