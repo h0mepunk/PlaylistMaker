2191652c9ab634985f774321e0d7029e785454ba
masha.kasatkina <mariia.kasatkina@dif.tech>
2025-11-30 14:49:40 +0400

    hw17 refactoring done fully

--- app/src/main/java/com/example/playlistmaker/ui/main/App.kt
+++ app/src/main/java/com/example/playlistmaker/ui/main/App.kt
@@ -6,7 +6,6 @@ import com.example.playlistmaker.di.interactorModule
 import com.example.playlistmaker.di.repositoryModule
 import com.example.playlistmaker.di.viewModelModule
 import com.example.playlistmaker.domain.api.ThemeInteractor
-import com.example.playlistmaker.util.Creator
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
