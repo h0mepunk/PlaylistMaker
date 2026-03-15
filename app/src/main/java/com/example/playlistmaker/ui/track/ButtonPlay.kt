0733d4c839cb9cd8bbc3bae0a28fd53f7d3f5040
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-03-15 14:27:55 +0400

    hw24 works fine

--- app/src/main/java/com/example/playlistmaker/ui/track/ButtonPlay.kt
+++ app/src/main/java/com/example/playlistmaker/ui/track/ButtonPlay.kt
@@ -1,12 +1,18 @@
 package com.example.playlistmaker.ui.track
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.core.graphics.drawable.toBitmap
 import com.example.playlistmaker.R
 import kotlin.math.min
 
@@ -16,10 +22,12 @@ internal class ButtonPlay @JvmOverloads constructor(
     @AttrRes defStyleAttr: Int = 0,
     @StyleRes defStyleRes: Int = 0,
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
-
-    val buttonImage: String
     private var state: Boolean = true
     private var isClicked: Boolean = false
+
+    private var imageBitmap: Bitmap? = null
+
+    private var imageRect: RectF = RectF()
 
     private val viewSize = resources.getDimensionPixelSize(
         R.dimen.media_round_button_big_button_size
@@ -34,6 +42,8 @@ internal class ButtonPlay @JvmOverloads constructor(
         }
     )
 
+    private var imageResIdPlay: Bitmap? = null
+    private var imageResIdPause: Bitmap? = null
 
     init {
         context.theme.obtainStyledAttributes(
@@ -43,14 +53,25 @@ internal class ButtonPlay @JvmOverloads constructor(
             defStyleRes
         ).apply {
             try {
-                buttonImage = getString(R.styleable.ButtonPlay_playButtonState) ?: ""
-                setImage(state)
+                imageResIdPlay = getDrawable(R.styleable.ButtonPlay_imageResIdPlay)?.toBitmap()
+                imageResIdPause = getDrawable( R.styleable.ButtonPlay_imageResIdPause)?.toBitmap()
             } finally {
                 recycle()
             }
         }
-
+        setImage(state)
         isEnabled = false
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        imageBitmap?.let {
+            canvas.drawBitmap(imageBitmap as Bitmap, null, imageRect, null)
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        imageRect = RectF(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat())
     }
 
     fun switchState() {
@@ -60,54 +81,6 @@ internal class ButtonPlay @JvmOverloads constructor(
 
     override fun setEnabled(enabled: Boolean) {
         super.setEnabled(enabled)
-    }
-
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        // Расчёт ширины
-        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
-        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
-        val contentWidth = when (widthMode) {
-            // Если ограничений на ширину нет —
-            // берём минимальное значение
-            MeasureSpec.UNSPECIFIED -> viewSize
-
-            // Если нужно указать точное значение ширины —
-            // берём это значение
-            MeasureSpec.EXACTLY -> viewSize
-
-            // Если можно указать не более widthSize —
-            // берём максимальную возможную ширину
-            MeasureSpec.AT_MOST -> widthSize
-
-            else -> error("Неизвестный режим ширины ($widthMode)")
-        }
-
-        // Расчёт высоты
-        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
-        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
-        val contentHeight = when (heightMode) {
-            // Если ограничений на высоту нет —
-            // берём минимальное значение
-            MeasureSpec.UNSPECIFIED -> viewSize
-
-            // Если нужно указать точное значение высоты —
-            // берём это значение
-            MeasureSpec.EXACTLY -> heightSize
-
-            // Если можно указать не более heightSize —
-            // берём максимальную возможную высоту
-            MeasureSpec.AT_MOST -> heightSize
-
-            else -> error("Неизвестный режим высоты ($heightMode)")
-        }
-
-        // Берём минимальное значение — либо ширину, либо высоту,
-        // чтобы сформировать квадрат.
-        val size = min(contentWidth, contentHeight)
-
-        // Устанавливаем посчитанные размеры
-        setMeasuredDimension(size, size)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -142,17 +115,13 @@ internal class ButtonPlay @JvmOverloads constructor(
     }
 
     private fun updateState() {
-       state = !state
+        state = !state
         setImage(state)
     }
 
     private fun setImage(state: Boolean) {
-        setBackgroundResource(
-            when (state) {
-                true -> R.drawable.media_play
-                false -> R.drawable.media_stop
-            }
-        )
+        imageBitmap = if (state) imageResIdPlay else imageResIdPause
+        invalidate()
     }
 
 }
